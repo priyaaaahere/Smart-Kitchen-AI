a7ad765ef990457bfe2e441dbd84d8fcd9e3e239
priyaaaahere <priyadarshinimohapatra1023@gmail.com>
2025-10-25 20:07:44 +0530

    xml AIChatFragment

--- app/src/main/java/com/example/smartkitchenai/ui/AIChatFragment.kt
+++ app/src/main/java/com/example/smartkitchenai/ui/AIChatFragment.kt
@@ -28,7 +28,7 @@ import kotlinx.coroutines.withContext
 
 class AIChatFragment : Fragment() {
 
-    private val key = ""
+    private val key = "AIzaSyAbdmAR_mCE67foExIJf9sNVZIhbzQUO7U"
 
     private lateinit var messageContainer: LinearLayout
     private lateinit var messageInput: EditText
